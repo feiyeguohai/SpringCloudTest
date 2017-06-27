77bc385827a0d934d8b0fada9da3cc1a9604c159
york1 <york1@huawei.com>
2017-06-27 20:03:11 +0800

    add config server project

---
+++ service-config-server/src/main/java/com/cloud/configServer/ServiceConfigServerApplication.java
@@ -0,0 +1,30 @@
+package com.cloud.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ServiceConfigServerApplication {
+    
+    
+    /*
+     * 
+     
+     URL与配置文件的映射关系如下：
+
+/{application}/{profile}[/{label}]
+/{application}-{profile}.yml
+/{label}/{application}-{profile}.yml
+/{application}-{profile}.properties
+/{label}/{application}-{profile}.properties
+上面的url会映射{application}-{profile}.properties对应的配置文件，{label}对应git上不同的分支，默认为master。
+     
+     
+     */
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConfigServerApplication.class, args);
+	}
+}
